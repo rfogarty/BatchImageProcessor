4e6adeb5b5a2001104eaa6165be3d987982c6101
Ryan Fogarty <ryan.fogarty@gmail.com>
2020-09-29 14:04:43 -0400

    Tested with Valgrind and AddressSanetizer

--- iptools/image/PixelConversion.h
+++ iptools/image/PixelConversion.h
@@ -32,39 +32,29 @@ void rgba2hsi(const RGBAPixel<ChannelT1,ChannelT1Traits>& rgba,
 
    double numerator = 0.5 * ((r - g) + (r - b));
    double denominator = std::sqrt((r-g)*(r-g) + (r-b)*(g-b));
-   // Kludge 3: if numerator > denominator set them equal!!
+   // Kludge: because of numeric instability, in rare circumstances,
+   // numerator > denominator, which results in a ratio > 1.0, but the
+   // ratio *needs* to be <= 1.0 o.w. acos is undefined (and results in
+   // nan. This isn't just hypothetical, experiments showed a few rare
+   // cases where this happens. Easiest solution? 
+   // if numerator > denominator set them equal!!
    if(numerator > denominator) numerator = denominator;
-   //std::cout << "r,g,b,n,d=" << r << "," << g << "," << b << "," << numerator << "," << denominator << std::endl;
 
    double h = denominator != 0 ? std::acos(numerator/denominator) : 0.0;
    if(b > g) h = twoPi() - h;
 
    // Because rgb are normalized by their sum
-   //double s = std::max(1.0 - 3.0*std::min(r,std::min(g,b)),0.0);
    double s = 1.0 - 3.0*std::min(r,std::min(g,b));
-   //double s = sumRGB > 0 ? (1.0 - std::min(r,std::min(g,b))/std::max(r,std::max(g,b))) : 0.0;
    double i = sumRGB/(3*ChannelT1Traits::max());
-
-   //std::cout << "h,su,i=" << h << "," << s << "," << i << std::endl;
 
    hsi.namedColor.hue = static_cast<ChannelT2>(h);
    hsi.namedColor.intensity = static_cast<ChannelT2>(i);
    // The above s is "unnormalized saturation", we need to now "normalize"
-   // s before saving it in HSIPixel.
-#if 1
-   //if(twoThirds() < i && i < 1.0) hsi.namedColor.saturation = s /(1.0 - 3.0*(i - twoThirds()));
+   // s before saving it in HSIPixel. This compensation on saturation ensures
+   // its space is valid from (0,1) throughout the intensity and hue space.
+   // This compensation needs to be undone right before conversion back to RGB space.
    if((twoThirds() < i) && (i < 1.0)) hsi.namedColor.saturation = s /(2.0/i - 2.0);
    else hsi.namedColor.saturation = s;
-//   if(0.0 < i && i <= 0.5) hsi.namedColor.saturation = static_cast<ChannelT2>(s/(2.0*i));
-//   else if(0.5 < i && i < 1.0) hsi.namedColor.saturation = static_cast<ChannelT2>(s/(2.0*(1.0-i)));
-//   // These next two cases should be i == 0.0, and i == 1.0 respectively, but
-//   // floating point comparisons should never be made by equivalence.
-//   else if(i < 0.5) hsi.namedColor.saturation = static_cast<ChannelT2>(0.0);
-//   else /* if(i > 0.5) */ hsi.namedColor.saturation = static_cast<ChannelT2>(0.0);
-#else
-   hsi.namedColor.saturation = static_cast<ChannelT2>(s);
-#endif
-   //std::cout << "h,sn,i=" << h << "," << hsi.namedColor.saturation << "," << i << std::endl;
 }
 
 template<typename ChannelT,typename ChannelTTraits>
@@ -76,7 +66,6 @@ inline void assignRGB(RGBAPixel<ChannelT,ChannelTTraits>& rgba,
    // underlying models have NOT taken this into account. As a result, we need to scale
    // by a small amount under the ChannelT range (max) - thus explains the odd addition
    // of a number close to 1 below.
-   //std::cout << "r,g,b=" << r << "," << g << "," << b << std::endl;
    rgba.namedColor.red   = static_cast<ChannelT>(r * (ChannelTTraits::max()+0.999));
    rgba.namedColor.green = static_cast<ChannelT>(g * (ChannelTTraits::max()+0.999));
    rgba.namedColor.blue  = static_cast<ChannelT>(b * (ChannelTTraits::max()+0.999));
@@ -99,47 +88,31 @@ void hsi2rgba(const HSIPixel<ChannelT1,ChannelT1Traits>& hsi,
    double h = hsi.namedColor.hue;
    double i = hsi.namedColor.intensity;
 
-   //std::cout << "h,sn,i=" << h << "," << hsi.namedColor.saturation << "," << i << std::endl;
    // Before implementing the standard conversion, we need to convert normalized saturation
    // to "unnormalized".
-#if 1
    double s = hsi.namedColor.saturation;
    if(twoThirds() < i) s *= (2.0/i - 2.0);
-   //if(twoThirds() < i && i < 1.0) s *= 1.0 - 3.0*(i - twoThirds());
-//   double s = 0.0;
-//   if(i <= 0.5) s = hsi.namedColor.saturation * 2.0 * i;
-//   else /* (i > 0.5) */ s = hsi.namedColor.saturation * 2.0 * (1.0 - i);
-   //std::cout << "h,su,i=" << h << "," << s << "," << i << std::endl;
-#else
-   double s = hsi.namedColor.saturation;
-#endif
+   
    double h120 = (h < twoThirdsPi()) ? h : ((h < fourThirdsPi()) ? (h - twoThirdsPi()) : (h - fourThirdsPi()) );
-
-   //std::cout << "h,h120=" << h << "," << h120 << std::endl;
 
    double x = i * (1.0 - s);
    double invHue = std::cos(h120) /
                    std::cos(oneThirdsPi() - h120);
    double y = i * (1.0 +  s * invHue);
-   // Kludge compensation
+   
+   // Due to numeric imprecision, in some cases, factor y
+   // can exceed 1.0. If we do no compensate for that the color
+   // space can wrap around causing extreme artifacts during HSI
+   // to RGB conversion. Kludge compensation
    if(y > 1.0) {
       // Solve for s that would make y = 1.0
+      // so that variables x and z will also be
+      // compensated properly.
       y = 1.0;
       s = (y/i - 1.0)/invHue;
       x = i * (1.0 - s);
    }
    double z = std::max(3.0*i - (x + y),0.0);
-
-   // if we increase intensity, without adjusting anything else, we could
-   // potentially get a y value that exceeds 1, in order to compensate,
-   // if y > 1.0, we will normalize all components by y.
-//   if(y > 1.0) {
-//      y = 1.0;
-//      x /= y;
-//      z /= y;
-//   }
-
-   //std::cout << "x,y,z=" << x << "," << y << "," << z << std::endl;
 
    if(h < twoThirdsPi()) assignRGB(rgba,y,z,x);
    else if(h < fourThirdsPi()) assignRGB(rgba,x,y,z);
